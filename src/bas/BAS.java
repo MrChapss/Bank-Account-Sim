1bbb488573237e43e464b54f9541a2e7902dcb40
Chapss <palmakarsten@gmail.com>
2025-10-20 01:18:41 +0800

    figuring out on how to do it

---
+++ src/bas/BAS.java
@@ -0,0 +1,11 @@
+package bas;
+import java.util.*;
+
+public class BAS {
+	public static void main(String[] args) {
+		// initialize the scanner
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+}
